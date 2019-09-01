cbae392ee5b669fb842751fdfe2f31df98d44098
einzigartigerName <nelson.gillo@gmx.de>
2019-09-01 23:23:23 +0200

    Add: print_uint32 and basic concept tested
    Bug: at the moment the file is not overwritten but new value is beeing
    appended

--- xlight.s
+++ xlight.s
@@ -14,9 +14,7 @@
 
 
 section .data
-    o_file db "output.txt",0
-    i_file db "input.txt", 0
-    prompt db "Enter Text (max 31 Chars)", 10
+    file db "test.txt",0
 
 section .bss
     input resb 32
@@ -25,50 +23,45 @@ section .text
     global _start
 
 _start:
-    ; open input-file
+    ; open file
     mov     rax, 2          ; sys_open = 2
-    mov     rdi, i_file     ; file = filename
-    mov     rsi, 0          ; O_RDONLY
+    mov     rdi, file       ; file = filename
+    mov     rsi, 66         ; O_RDWR + O_CREATE
+    mov     rdx, 0644o      ; permisions
     syscall
 
-    push    rdi             ; save fd
+    mov     rbx, rax             ; save fd
 
-    ; Read from stdin
+    ; Read from file
     mov     rdi, rax        ; fd from input-file
     mov     rax, 0          ; sys_read = 0
     mov     rsi, input      ; buffer
     mov     rdx, 32         ; buffer length
     syscall
 
-    pop     rdi             ; get fd from stack to close it
-    push    rax             ; Save read bytes
-    mov     rax, 3          ; sys_close = 3
-    syscall
+    ; convert buffer to uint32
+    mov     rdi, input      ; buffer to convert
+    call    ascii_to_int    
 
-    ; open output-file
-    mov     rax, 2          ; sys_open = 2
-    mov     rdi, o_file     ; file = filename
-    mov     rsi, 65         ; O_WONLY = 1 + O_CREATE = 64
-    mov     rdx, 0644o      ; permisions
-    syscall
+    add     rax, 100        ; add 100 to number
 
-    ; print buffer 
-    mov     rdi, rax        ; fd to write to
-    pop     rdx             ; lenght to write
-    push    rax             ; save fd
-    mov     rax, 1          ; sys_write = 1
-    mov     rsi, input      ; buffer to print
-    syscall
+    ; write new number to file
+    mov     rdi, rax        ; uint32 to write
+    mov     rsi, rbx        ; fd to write number to
+    call    print_uint32
 
     ; close file
     mov     rax, 3          ; sys_close = 3
-    pop     rdi             ; pop fd from stack
+    mov     rdi, rbx        ; file to close
     syscall
 
     ; Exit
     mov     rax, 60         ; sys_exit = 60
     mov     rdi, 0          ; return value
     syscall
+
+
+
 
 ; rdi:  pointer to ascii-string
 ; function terminates at first non digit
@@ -96,3 +89,38 @@ ascii_to_int:
     jbe     .prep_next          ; add value
     
     ret                         ; else return current total
+
+
+
+
+; rdi:  uint32
+; rsi:  fd to write to
+print_uint32:
+    mov     r8, rsi             ; save fd
+    mov     eax, edi            ; uint32 value moved for div
+    mov     ecx, 10             ; base 10 = \n
+    push    rcx                 
+    mov     rsi, rsp            ; save stack-pointer
+    sub     rsp, 16
+
+
+
+.digit_to_ascii:
+    xor     edx, edx            ; clear edx
+    div     ecx                 ; eax/10 with remainder in edx
+    add     edx, 48             ; digit to ascii
+    dec     rsi
+    mov     [rsi], dl            ; put digit into write buffer
+
+    test    eax, eax            ; check if 0
+    jnz     .digit_to_ascii     ; continue loop
+
+    ; else write
+    mov     rax, 1              ; sys_write
+    mov     rdi, r8             ; fd to write to
+    lea     rdx, [rsp + 16 + 1] ; get length
+    sub     rdx, rsi            ; lenght with \n
+    syscall
+
+    add     rsp, 24             ; undo buffer
+    ret
